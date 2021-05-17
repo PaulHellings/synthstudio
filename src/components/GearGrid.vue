6d9d9f9234c33b37950d119127245f68e20d2bfa
Paul Hellings <info@paulhellings.nl>
2021-05-17 21:58:01 +0200

    Add simple gear grid

---
+++ src/components/GearGrid.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-data-table
+      :headers="headers"
+      :items="gear"
+      class="elevation-1"
+  ></v-data-table>
+</template>
+
+<script>
+export default {
+  name: 'GearGrid',
+
+  data() {
+    return {
+      headers: [
+        {
+          text: 'Model',
+          value: 'model',
+        },
+        {
+          text: 'Brand',
+          value: 'brand',
+        },
+        {
+          text: 'Type',
+          value: 'type',
+        },
+      ],
+      gear: [
+        {
+          id: 1,
+          type: 'synthesizer',
+          model: 'Virus',
+          brand: 'Access',
+        },
+        {
+          id: 2,
+          type: 'synthesizer',
+          model: 'Streichfett',
+          brand: 'Waldorf',
+        },
+        {
+          id: 3,
+          type: 'drummachine',
+          model: 'Analog Rytm',
+          brand: 'Elektron',
+        },
+        {
+          id: 4,
+          type: 'drummachine',
+          model: 'RD-8',
+          brand: 'Behringer',
+        },
+        {
+          id: 5,
+          type: 'synthesizer',
+          model: 'Quasar',
+          brand: 'Quasimidi',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="home">
     Synths and cats are cool
+
+    <gear-grid></gear-grid>
   </div>
 </template>
 
 <script>
 
+import GearGrid from '@/components/GearGrid.vue';
 export default {
   name: 'Home',
-  components: {},
+  components: { GearGrid },
 };
 </script>
