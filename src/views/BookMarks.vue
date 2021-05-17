23f2c4601ac3eef1976cee4686af4238e1aa18fa
Paul Hellings <info@paulhellings.nl>
2021-05-17 23:43:56 +0200

    add a few bookmarks in a simple list

--- src/views/BookMarks.vue
+++ src/views/BookMarks.vue
@@ -1,10 +1,36 @@
 <template>
-  <div>Bookmarks</div>
+  <v-container fluid>
+    <v-list>
+      <v-list-item v-for="bookmark in bookmarks" :key="bookmark.id">
+        <v-list-item-content>
+          <v-list-item-title><a class="text-decoration-none" :href="bookmark.url" target="_blank">{{ bookmark.name }}</a></v-list-item-title>
+          <v-list-item-subtitle>{{ bookmark.url }}</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-container>
 </template>
 
 <script>
 export default {
   name: 'BookMarks',
+
+  data() {
+    return {
+      bookmarks: [
+        {
+          id: 0,
+          name: 'Synthforum',
+          url: 'http://www.synthforum.nl',
+        },
+        {
+          id: 1,
+          name: 'Modulargrid',
+          url: 'http://www.modulargrid.net',
+        },
+      ],
+    };
+  },
 };
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container fluid>
-    Synths and cats are cool
+    <span>Synths and cats are cool</span>
     <gear-grid></gear-grid>
   </v-container>
 </template>
